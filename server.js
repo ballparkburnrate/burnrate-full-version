af562ed6bdedf0e45c664fea571d8ff72118fb4e
ednguyen <ednguyen92@gmail.com>
2018-06-16 12:36:18 -0700

    Added Working Initial Cash

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(passport.session()); // persistent login sessions
 
 //For Handlebars
 app.set('views', './views')
-app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: "main"}));
+app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 
