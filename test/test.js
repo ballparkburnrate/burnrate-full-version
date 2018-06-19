bb55f4e2b5e699f11487034374400f47c2fd59c7
Maribel Jaramillo <marijar84@gmail.com>
2018-06-18 21:36:00 -0700

    Add Unit test

--- test/test.js
+++ test/test.js
@@ -10,6 +10,25 @@ chai.use(chaiHttp);
 
 console.log("Start Get User");
 
+// describe('Signin', function () {
+// it('should redirect to dashboard on successful login', function(done) {
+//     chai.request('http://localhost:3000')
+//       .post('/signin')
+//       .set('Token', 'text/plain')
+//       .set('content-type', 'application/x-www-form-urlencoded')
+//       .type('form')
+//       .send('grant_type=password')
+//       .send('email=martinash@gmail.com')
+//       .send('password=1234')
+//       .end(function(err, res) {
+//           console.log(res);
+//         res.should.have.status(200);
+//         //expect(res).to.redirectTo('http://localhost:3000/index');
+//         done();
+//       });
+//   });
+// });
+
 describe('User', function () {
     const userCredentials = {
         firstname: "Test",
@@ -40,22 +59,22 @@ describe('User', function () {
                         "UserId": idUser
                     };
 
-                    describe('Create Cash', function () {
-                        it('responds with status 200 and insert cash', function (done) {
-                            chai.request("http://localhost:3000")
-                                .post('/api/cash')
-                                .send(cash)
-                                .end(function (err, res) {
-                                    expect(res).to.have.status(200);
-                                    done();
-                                });
-                        });
-                    });
+                    // describe('Create Cash', function () {
+                    //     it('responds with status 200 and insert cash', function (done) {
+                    //         chai.request("http://localhost:3000")
+                    //             .post('/api/cash')
+                    //             .send(cash)
+                    //             .end(function (err, res) {
+                    //                 expect(res).to.have.status(200);
+                    //                 done();
+                    //             });
+                    //     });
+                    // });
 
                     describe('Get Cash by User', function () {
                         it('responds with status 200 and get data', function (done) {
                             chai.request("http://localhost:3000")
-                                .get('/api/cash/by-user/' + idUser)
+                                .get('/api/cash/by-user/' + 1)
                                 .end(function (err, res) {
                                     console.log("Cash list length " + res.body.length);
                                     expect(res).to.have.status(200);
@@ -66,42 +85,84 @@ describe('User', function () {
                     });
 
 
-                    const income = {
-                        "isRecurring" : 0,
-                        "amount": 30000,
-                        "customer" : "Christian M",
-                        "description" : ""
-                    };
+                    // const income = {
+                    //     "isRecurring" : 0,
+                    //     "amount": 30000,
+                    //     "customer" : "Christian M",
+                    //     "description" : ""
+                    // };
 
-                    var User = {
-                        id: idUser                        
-                    };
+                    // var User = {
+                    //     id: idUser                        
+                    // };
 
-                    describe('Create Income', function () {
-                        it('responds with status 200 and insert income', function (done) {
+                    // describe('Create Income', function () {
+                    //     it('responds with status 200 and insert income', function (done) {
+                    //         chai.request("http://localhost:3000")
+                    //             .post('/api/incomes')
+                    //             .send(income) 
+                    //             .send(User)                               
+                    //             .end(function (err, res) {
+                    //                 expect(res).to.have.status(200);
+                    //                 done();
+                    //             });
+                    //     });
+                    // });
+
+                    describe('Get Income by User group by customer', function () {
+                        it('responds with status 200 and get data', function (done) {
                             chai.request("http://localhost:3000")
-                                .post('/api/incomes')
-                                .send(income) 
-                                .send(User)                               
+                                .get('/api/incomes/by-customer/' + 1)
                                 .end(function (err, res) {
+                                    console.log("Income list length " + res.body.length);
                                     expect(res).to.have.status(200);
+                                    expect(res.body.length).to.be.at.least(0);
                                     done();
                                 });
                         });
                     });
 
-                    // describe('Get Income by User group by customer', function () {
-                    //     it('responds with status 200 and get data', function (done) {
+
+
+
+
+                    // const expense = {
+                    //     "isRecurring" : 0,
+                    //     "amount": 30000,
+                    //      "category": "Employees"
+                    //     "vendor" : "Christian M",
+                    //     "description" : ""
+                    // };
+
+                    // var User = {
+                    //     id: idUser                        
+                    // };
+
+                    // describe('Create Income', function () {
+                    //     it('responds with status 200 and insert income', function (done) {
                     //         chai.request("http://localhost:3000")
-                    //             .get('/api/incomes/by-customer/' + idUser)
+                    //             .post('/api/expenses')
+                    //             .send(income) 
+                    //             .send(User)                               
                     //             .end(function (err, res) {
-                    //                 console.log("Income list length " + res.body.length);
                     //                 expect(res).to.have.status(200);
-                    //                 expect(res.body.length).to.be.at.least(0);
                     //                 done();
                     //             });
                     //     });
                     // });
+
+                    describe('Get Expense by User group by category', function () {
+                        it('responds with status 200 and get data', function (done) {
+                            chai.request("http://localhost:3000")
+                                .get('/api/expenses/by-category/' + 1)
+                                .end(function (err, res) {
+                                    console.log("Income list length " + res.body.length);
+                                    expect(res).to.have.status(200);
+                                    expect(res.body.length).to.be.at.least(0);
+                                    done();
+                                });
+                        });
+                    });
                 });
         });
     });
